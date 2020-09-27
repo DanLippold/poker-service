9283ee9fc001a7f4c374a9cfa66bff2d3b5bc44a
Dan <dlippold08@gmail.com>
2020-09-27 18:50:51 -0500

    feat(*): add prettier and eslint

--- libs/pokerService.js
+++ libs/pokerService.js
@@ -1,67 +1,70 @@
-
-const Table = require('./table');
-const TurnRequest = require('./turnRequest');
-const Logger = require('./logger');
+const Table = require("./table");
+const TurnRequest = require("./turnRequest");
+const Logger = require("./logger");
 
 const DEFAULT_CHIPS_PER_PLAYER = 10000;
 
 class PokerService {
-    constructor(options) {
-        if (!options.players || options.players.length < 2) { // TODO: players should probably be a mandatory first param
-            throw new Error('2 or more players need to be defined in options');
-        }
-
-        options = Object.assign({
-            chips: DEFAULT_CHIPS_PER_PLAYER
-        }, options);
-
-        this.playerCount = options.players.length;
-        this.players = options.players;
-        this.logging = options.logging;
-
-        if (this.logging) {
-            this.logger = new Logger();
-            logger.setOptions(options);
-        }
-
-        this.table = new Table(this.players, options.chips, options.seed);
-        this.tableActions = {
-            FOLD: 'fold',
-            CALL: 'call',
-            BET: 'bet',
-            RAISE: 'raise',
-            CHECK: 'check'
-        }
-        this.setTurnRequest();
+  constructor(options) {
+    if (!options.players || options.players.length < 2) {
+      // TODO: players should probably be a mandatory first param
+      throw new Error("2 or more players need to be defined in options");
     }
 
-    setTurnRequest() {
-        this.activeTurnRequest = new TurnRequest();
+    options = Object.assign(
+      {
+        chips: DEFAULT_CHIPS_PER_PLAYER,
+      },
+      options
+    );
 
-        this.activeTurnRequest.playerName = this.table.activePlayer.name;
-        this.activeTurnRequest.playerCards = this.table.activePlayer.cards;
-        this.activeTurnRequest.playerChipValue = this.table.activePlayer.chipValue;
-        this.activeTurnRequest.players = this.players;
-        this.activeTurnRequest.boardCards = this.table.board;
-        this.activeTurnRequest.playerPosition = this.table.activePlayerIndex;
-        this.activeTurnRequest.activeBetValue = this.table.activeBetValue;
-        this.activeTurnRequest.potValue = this.table.potValue;
-        this.activeTurnRequest.handNumber = this.table.handNumber;
+    this.playerCount = options.players.length;
+    this.players = options.players;
+    this.logging = options.logging;
+
+    if (this.logging) {
+      this.logger = new Logger();
+      this.logger.setOptions(options);
     }
 
-    act(turn) {
-        if (!this.table.validTurn(turn.type, turn.amount)) {
-            return this.activeTurnRequest; // Probably should return an error
-        }
-        this.table[this.tableActions[turn.type]](turn.amount);
-        if (this.logging) {
-            this.logger.logTurnRequest(this.activeTurnRequest);
-            this.logger.logAction(turn);
-        }
-        this.setTurnRequest();
+    this.table = new Table(this.players, options.chips, options.seed);
+    this.tableActions = {
+      FOLD: "fold",
+      CALL: "call",
+      BET: "bet",
+      RAISE: "raise",
+      CHECK: "check",
+    };
+    this.setTurnRequest();
+  }
 
-        return this.activeTurnRequest;
+  setTurnRequest() {
+    this.activeTurnRequest = new TurnRequest();
+
+    this.activeTurnRequest.playerName = this.table.activePlayer.name;
+    this.activeTurnRequest.playerCards = this.table.activePlayer.cards;
+    this.activeTurnRequest.playerChipValue = this.table.activePlayer.chipValue;
+    this.activeTurnRequest.players = this.players;
+    this.activeTurnRequest.boardCards = this.table.board;
+    this.activeTurnRequest.playerPosition = this.table.activePlayerIndex;
+    this.activeTurnRequest.activeBetValue = this.table.activeBetValue;
+    this.activeTurnRequest.potValue = this.table.potValue;
+    this.activeTurnRequest.handNumber = this.table.handNumber;
+  }
+
+  act(turn) {
+    if (!this.table.validTurn(turn.type, turn.amount)) {
+      return this.activeTurnRequest; // Probably should return an error
     }
+    this.table[this.tableActions[turn.type]](turn.amount);
+    if (this.logging) {
+      this.logger.logTurnRequest(this.activeTurnRequest);
+      this.logger.logAction(turn);
+    }
+    this.setTurnRequest();
+
+    return this.activeTurnRequest;
+  }
 }
 
 module.exports = PokerService;
--- libs/table.js
+++ libs/table.js
@@ -1,99 +1,125 @@
-const Dealer = require('./dealer');
-const { getWinningPlayers } = require('./handEvaluator');
-const { getNextActivePlayerFromIndex } = require('./players');
-const handEvaluator = require('./handEvaluator');
+const Dealer = require("./dealer");
+const { getWinningPlayers } = require("./handEvaluator");
+const { getNextActivePlayerFromIndex } = require("./players");
+const handEvaluator = require("./handEvaluator");
 
 class Table {
-    constructor(players, startingChips, seed) {
-        this.players = players;
-        this.handNumber = 0;
-        this.dealer = new Dealer(seed);
-        this.dealer.distributeChips(this.players, startingChips);
-        this.dealer.setRandomDealer(this, this.players);
+  constructor(players, startingChips, seed) {
+    this.players = players;
+    this.handNumber = 0;
+    this.dealer = new Dealer(seed);
+    this.dealer.distributeChips(this.players, startingChips);
+    this.dealer.setRandomDealer(this, this.players);
+    this._startNextHand(this.dealerPosition, this.players);
+  }
+
+  _startNextHand(dealerPosition, players) {
+    this.dealer.clearBoard(this);
+    this.dealer.clearPot(this);
+    this.dealer.clearActiveBets(this, true);
+    this.dealer.setBlindPositions(this);
+    this.dealer.collectBlinds(this, players);
+    this.dealer.dealNewCardsToEachPlayer(this, players, dealerPosition);
+    this.dealer.setFirstPlayerToAct(
+      this,
+      players,
+      dealerPosition,
+      this.bigBlindPosition,
+      this.board
+    ); // TODO: This feels dumb
+    this.handNumber++;
+  }
+
+  // All players have matched each other's bets, folded, or are out of chips
+  _potIsGood() {
+    return this.players
+      .map((player) => {
+        return (
+          player.activeBetValue === -2 ||
+          player.chipValue === 0 ||
+          player.activeBetValue === this.activeBetValue
+        );
+      })
+      .every(Boolean);
+
+    // return true;
+    // const everyoneChecked = this.bettingPlayerIndex === -1;
+    // const dealerJustActed = previousActivePlayerIndex === this.dealerPosition;
+    // const everyoneCalledOrFolded = this.activePlayerIndex === this.bettingPlayerIndex;
+
+    // return (everyoneChecked && dealerJustActed) || everyoneCalledOrFolded;
+  }
+
+  _endTurn() {
+    const previousActivePlayerIndex = this.activePlayerIndex;
+    this.activePlayer = getNextActivePlayerFromIndex(
+      this.players,
+      this.activePlayerIndex
+    );
+    this.activePlayerIndex = this.players.indexOf(this.activePlayer);
+
+    if (this._potIsGood(previousActivePlayerIndex)) {
+      if (this.board.length === 5) {
+        // End of Hand
+        const winningPlayers = getWinningPlayers(this.players, this.board);
+        winningPlayers[0].chipValue += this.potValue; // TODO: Handle split pot
+        const newDealerPlayer = getNextActivePlayerFromIndex(
+          this.players,
+          this.dealerPosition
+        );
+        this.dealerPosition = this.players.indexOf(newDealerPlayer);
         this._startNextHand(this.dealerPosition, this.players);
+      } else {
+        this.dealer.clearActiveBets(this, false);
+        this.dealer.dealStreet(this.board, this.deck);
+        this.dealer.setFirstPlayerToAct(
+          this,
+          this.players,
+          this.dealerPosition,
+          this.bigBlindPosition,
+          this.board
+        );
+      }
     }
 
-    _startNextHand(dealerPosition, players) {
-        this.dealer.clearBoard(this);
-        this.dealer.clearPot(this);
-        this.dealer.clearActiveBets(this, true);
-        this.dealer.setBlindPositions(this);
-        this.dealer.collectBlinds(this, players);
-        this.dealer.dealNewCardsToEachPlayer(this, players, dealerPosition);
-        this.dealer.setFirstPlayerToAct(this, players, dealerPosition, this.bigBlindPosition, this.board); // TODO: This feels dumb
-        this.handNumber++;
-    }
+    this.players.forEach((player) => {
+      player.pokerHand = handEvaluator.getHandDescription(
+        player.cards,
+        this.board
+      );
+    });
+  }
 
-    // All players have matched each other's bets, folded, or are out of chips
-    _potIsGood() {
-        return this.players.map((player) => {
-            return player.activeBetValue === -2 ||
-                player.chipValue === 0 ||
-                player.activeBetValue === this.activeBetValue;
-        }).every(Boolean);
+  raise(amount) {
+    // TODO: Validate raise
+    this.dealer.handleRaise(amount, this);
+    this._endTurn();
+  }
 
-        return true;
-        // const everyoneChecked = this.bettingPlayerIndex === -1;
-        // const dealerJustActed = previousActivePlayerIndex === this.dealerPosition;
-        // const everyoneCalledOrFolded = this.activePlayerIndex === this.bettingPlayerIndex;
+  bet(amount) {
+    // TODO: Validate bet
+    this.raise(amount); // A bet is really just a raise on a $0 active bet
+  }
 
-        // return (everyoneChecked && dealerJustActed) || everyoneCalledOrFolded;
-    }
+  call() {
+    // TODO: Validate call
+    this.raise(0); // A call is really just a raise of $0
+  }
 
-    _endTurn() {
-        const previousActivePlayerIndex = this.activePlayerIndex;
-        this.activePlayer = getNextActivePlayerFromIndex(this.players, this.activePlayerIndex);
-        this.activePlayerIndex = this.players.indexOf(this.activePlayer);
+  check() {
+    // TODO: Validate check
+    this.raise(0); // A check is really just a raise of $0 on a $0 active bet
+  }
 
-        if (this._potIsGood(previousActivePlayerIndex)) {
-            if (this.board.length === 5) { // End of Hand
-                const winningPlayers = getWinningPlayers(this.players, this.board);
-                winningPlayers[0].chipValue += this.potValue; // TODO: Handle split pot
-                const newDealerPlayer = getNextActivePlayerFromIndex(this.players, this.dealerPosition);
-                this.dealerPosition = this.players.indexOf(newDealerPlayer);
-                this._startNextHand(this.dealerPosition, this.players);
-            } else {
-                this.dealer.clearActiveBets(this, false);
-                this.dealer.dealStreet(this.board, this.deck);
-                this.dealer.setFirstPlayerToAct(this, this.players, this.dealerPosition, this.bigBlindPosition, this.board);
-            }
-        }
+  fold() {
+    this.activePlayer.activeBetValue = -2;
+    this.activePlayer.cards = [];
+    this._endTurn();
+  }
 
-        this.players.forEach(player => {
-            player.pokerHand = handEvaluator.getHandDescription(player.cards, this.board);
-        })
-    }
-
-    raise(amount) {
-        // TODO: Validate raise
-        this.dealer.handleRaise(amount, this);
-        this._endTurn();
-    }
-
-    bet(amount) {
-        // TODO: Validate bet
-        this.raise(amount); // A bet is really just a raise on a $0 active bet
-    }
-
-    call() {
-        // TODO: Validate call
-        this.raise(0); // A call is really just a raise of $0
-    }
-
-    check() {
-        // TODO: Validate check
-        this.raise(0); // A check is really just a raise of $0 on a $0 active bet
-    }
-
-    fold() {
-        this.activePlayer.activeBetValue = -2;
-        this.activePlayer.cards = [];
-        this._endTurn();
-    }
-
-    validTurn() {
-        return true;
-    }
+  validTurn() {
+    return true;
+  }
 }
 
 module.exports = Table;
